5bc621bf49fd8161fa0cc553cde4ca429a1e9b14
PIPPIRIPPIP <ayeshaalifirdaus23@gmail.com>
2023-03-07 01:14:37 +0700

    janji cpp

--- CPP/Program/human.cpp
+++ CPP/Program/human.cpp
@@ -1,3 +1,6 @@
+// Saya Ayesha Ali Firdaus (NIM 2101990) mengerjakan evaluasi LATIHAN 4 dalam mata kuliah Desain Pemrograman Berorientasi Objek
+// untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+
 #include "header.hh"
 using namespace std;
 
